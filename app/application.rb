9328b9c4f36656bd08c13956da7854f7aafe19ab
Adam Urbina <adamurbina@gmail.com>
2018-07-31 14:29:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,16 +1,17 @@
 class App
-    
+
     @@items = []
-    
+
     def call(env)
         resp = Rack::Response.new
         req = Rack::Request.new(env)
 
-        
+
     end
 
     def handle_request(item)
-        if item
+        @@items.select{|item| item.name = item}
+        if @@items.collect.include?(item)
             
         end
     end
