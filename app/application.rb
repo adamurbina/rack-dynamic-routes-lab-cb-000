a95375284e5d9c2b55c6f6a36d12d3781d01657d
Adam Urbina <adamurbina@gmail.com>
2018-07-31 14:23:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,17 @@
 class App
     
+    @@items = []
+    
+    def call(env)
+        resp = Rack::Response.new
+        req = Rack::Request.new(env)
+
+        
+    end
+
+    def handle_request(item)
+        if item
+            
+        end
+    end
 end
