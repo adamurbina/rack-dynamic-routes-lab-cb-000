5d9ab0196a5bc572367a4736af277b6096b02d4b
Adam Urbina <adamurbina@gmail.com>
2018-07-31 14:41:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -21,7 +21,7 @@ class Application
 
     def handle_request(search_item)
         item = @@items.select{|item| item.name = search_item}
-        if item
+        if item != []
             return "#{item[0].price}"
         else
             return "Item not found"
